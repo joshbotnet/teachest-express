2462f2aa3ac87850d17c20fd4e20b0de086954ea
Joshua Martin <josh@joshbot.net>
2016-12-21 22:14:55 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# TeaChest Express
+
+<p><img src="./static/teachest.png" width="220px" /></p>
+
+Express Server based on expressjs/express
+
+This is one of 3 related projects:
+
+- `VueJS App` - https://github.com/joshbotnet/teachest-vuejs.git
+- `Express Server` - https://github.com/joshbotnet/teachest-express.git
+- `GraphQL Server` - https://github.com/joshbotnet/teachest-graphql.git
+
+See the TeaChest Stack README for details:
+
+- https://github.com/joshbotnet/teachest-stack.git
+
+References for this project:
+
+- https://github.com/expressjs/express
---
+++ src/index.js
@@ -0,0 +1,113 @@
+import Debug from 'debug';
+import express from 'express';
+import request from 'request';
+
+const app = express();
+const debug = Debug('teachest-server:server');
+const http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+const port = normalizePort(process.env.PORT || '2000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+var sourceurl = 'http://tealight.io';
+
+app.use('/', function(req, res) {
+  var url = sourceurl + req.url;
+  req.pipe(request(url)).pipe(res);
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals
+  res.locals.message = err.message;
+  res.locals.error = err;
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
